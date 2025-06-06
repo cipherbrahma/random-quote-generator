7133c26de99376a9af49ed77cdf0a405048b7b24
cipherbrahma <anirudh.rozario@gmail.com>
2025-06-06 02:01:05 -0500

    Updated quote title and styles

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ const quotes = [
     "Manam Bagunapudu lekalu mataladi. Kasthalo unapudu viluvalu matladakudadu. – Viraj Anand",
     "Knowledge is Divine ante Wine entha tagithe antha manchidi. – Shivram Babai",
     "Athiga aasapade magaadu... athiga aavesapade aadadhi sukhapadinatu charithra lo ledu. – Narasimha",
-    "In the middle of every difficulty lies opportunity. – Einstein"
+    "Telisindi Gorantha. Teliyalsindhi Kondantha. – Baba",
+    "Rechakotku Nannu Rechagotku. – Godavari bullodu",
+    "Thine varaku istharaku antaru Thinesaka engilaku antaru inthe mowa – Vanki",
+    "Aadapilla ni aadapilla enduk antaro telusa? Aadapilla Eedapilla kadu kabati – Sharmila",
+    "Idly pindi tho dosa eyochu kani, Dosa pindi toh idly matram cheyalemu – Batasari",
+    "Perugannam lo peruguntundi kani, Pulihora lo puli undadu mowa – Anveshi",
+    "Gaalo vimanam samudram lo padutundemo kani, submarine matram epudu gaalo egaraledu – Salaar",
+    "Manam mokkaki neelu matrame poyal mowa, puvvu matram ashinchakudadu – Thandel Shankar",
+    "Punjochi pettani dengthe etluntado telsa, nen ochi kodthe atle untadi – Punju",
   ];
   
   function generateQuote() {
